04a884ee3c899e017902e45a9f0b3f5b8dc20fc3
lucenstuff <lucentiniagustin@hotmail.com>
2023-12-30 15:44:20 -0300

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
